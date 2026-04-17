3a48414f7afe3d42c0e0fdcf5574632aa3b58664
Choitaeung <choitaeung990221@gmail.com>
2026-04-17 11:41:13 +0900

    미완성 상담 모달 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,11 @@
 import { GNB } from '@/widgets/gnb';
+import { ChatNavigationTracker } from './ChatNavigationTracker';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <GNB />
+      <ChatNavigationTracker />
       <main className="bg-secondary-100 flex flex-1 flex-col box-border pt-16 md:pt-20">
         {children}
       </main>
